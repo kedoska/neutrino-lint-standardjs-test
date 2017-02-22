c1791df20ddf08433917dda41f057b8c26e6df4a
Marco Casula <mc@clickbit.net>
2017-02-22 18:47:14 -0500

    use neutrino-lint-base
    Signed-off-by: Marco Casula <mc@clickbit.net>

---
+++ README.md
@@ -0,0 +1,9 @@
+## Test
+
+1. `yarn` on root
+2. `cd neutrino-preset-standard-style`
+3. `yarn` again
+4. `cd ..`
+5. `yarn build`
+
+
--- neutrino-preset-standard-style/src/index.js
+++ neutrino-preset-standard-style/src/index.js
@@ -1,13 +1,27 @@
-const path = require('path')
+'use strict'
 
-module.exports = neutrino => {
-  neutrino.config.module
+const path = require('path')
+const lint = require('neutrino-lint-base')
+const merge = require('deepmerge')
+
+const MODULES = path.join(__dirname, '../node_modules')
+
+module.exports = (neutrino) => {
+  lint(neutrino)
+  const { config } = neutrino
+  config.module
     .rule('lint')
+    .test(/\.(js|jsx)$/)
     .pre()
-    .test(/\.jsx?$/)
     .include(path.join(process.cwd(), 'src'))
-    .loader('standard', require.resolve('standard-loader'), {
-      snazzy: false,
-      emitErrors: true
-    })
+    .loader('eslint', props => merge(props, {
+      options: {
+        plugins: ['babel'],
+        baseConfig: {
+          extends: ['standard', 'standard-jsx']
+        }
+      }
+    }))
+  config.resolve.modules.add(MODULES);
+  config.resolveLoader.modules.add(MODULES);
 }
