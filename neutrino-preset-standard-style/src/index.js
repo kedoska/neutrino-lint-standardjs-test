8cb9af72fc7c71257db396546293c5bae6f4c691
Marco Casula <mc@clickbit.net>
2017-02-22 19:10:36 -0500

    pass user configuration to loader
    Signed-off-by: Marco Casula <mc@clickbit.net>

--- neutrino-preset-standard-style/src/index.js
+++ neutrino-preset-standard-style/src/index.js
@@ -3,25 +3,26 @@
 const path = require('path')
 const lint = require('neutrino-lint-base')
 const merge = require('deepmerge')
+const pkg = require(path.join(process.cwd(), 'package.json'))
 
 const MODULES = path.join(__dirname, '../node_modules')
 
 module.exports = (neutrino) => {
   lint(neutrino)
   const { config } = neutrino
+  const { standard } = pkg
+  const options = merge({
+    plugins: ['babel'],
+    baseConfig: {
+      extends: ['standard', 'standard-jsx']
+    }
+  }, standard)
   config.module
     .rule('lint')
     .test(/\.(js|jsx)$/)
     .pre()
     .include(path.join(process.cwd(), 'src'))
-    .loader('eslint', props => merge(props, {
-      options: {
-        plugins: ['babel'],
-        baseConfig: {
-          extends: ['standard', 'standard-jsx']
-        }
-      }
-    }))
-  config.resolve.modules.add(MODULES);
-  config.resolveLoader.modules.add(MODULES);
+    .loader('eslint', props => merge(props, {options}))
+  config.resolve.modules.add(MODULES)
+  config.resolveLoader.modules.add(MODULES)
 }
